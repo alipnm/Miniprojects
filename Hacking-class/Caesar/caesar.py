beaf4eec7d7a803e67dff4bb32c937875119f95b
alipnm <alipnm110@gmail.com>
2025-10-15 16:24:10 +0330

    testing_caesar.py
    I changed some codes

--- Hacking-class/Caesar/caesar.py
+++ Hacking-class/Caesar/caesar.py
@@ -1,17 +1,21 @@
 class Caesar:
+    def __init__(self) -> None:
+        self.ord_A = ord('A')
+        self.ord_a = ord('a')
+
     def encrypt(self, text: str, shift: int) -> str:
         shift = shift % 26
         result = ""
 
         for c in text:
             if c.isupper():
-                current_position = ord(c) - ord('A')
-                new_position = (current_position + shift) % 26
+                current_position = ord(c) - self.ord_A
+                new_position = ((current_position + shift) % 26) + self.ord_A
                 new_char = chr(new_position)
                 result += new_char
             elif c.islower():
-                current_position = ord(c) - ord('a')
-                new_position = (current_position + shift) % 26
+                current_position = ord(c) - self.ord_a
+                new_position = ((current_position + shift) % 26) + self.ord_a
                 new_char = chr(new_position)
                 result += new_char
             else:
@@ -25,26 +29,30 @@ class Caesar:
 
         for c in text:
             if c.isupper():
-                current_position = ord(c) - ord('A')
+                current_position = ord(c) - self.ord_A
 
                 new_position_shifted = current_position - shift
                 if new_position_shifted < 0:
                     new_position_shifted *= -1
-                    new_position_shifted = 26 - shift
+                    new_position_shifted = (26 - shift) + self.ord_A
                 else:
-                    new_position_shifted = new_position_shifted % 26
+                    new_position_shifted = (
+                        new_position_shifted % 26
+                    ) + self.ord_A
 
                 new_char = chr(new_position_shifted)
                 result += new_char
             elif c.islower():
-                current_position = ord(c) - ord('A')
+                current_position = ord(c) - self.ord_a
 
                 new_position_shifted = current_position - shift
                 if new_position_shifted < 0:
                     new_position_shifted *= -1
-                    new_position_shifted = 26 - shift
+                    new_position_shifted = (26 - shift) + self.ord_a
                 else:
-                    new_position_shifted = new_position_shifted % 26
+                    new_position_shifted = (
+                        new_position_shifted % 26
+                    ) + self.ord_a
 
                 new_char = chr(new_position_shifted)
                 result += new_char
