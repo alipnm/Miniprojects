b6fc0a824377a3193aa055ca92c8820d08e36b05
alipnm <alipnm110@gmail.com>
2025-11-07 19:36:42 +0330

    Finishing Hash Project and initializing README

--- Hacking-class/Hash/forms/main_form.py
+++ Hacking-class/Hash/forms/main_form.py
@@ -1,4 +1,6 @@
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QMessageBox
+import pyperclip
+from hash_machine import HashMachine
 from ui.ui_main import Ui_Encrypt
 
 
@@ -8,3 +10,17 @@ class MainForm(QDialog):
 
         self.ui = Ui_Encrypt()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.encrypt)
+
+    def encrypt(self):
+        text = self.ui.lineEdit.text()
+        sha_func = self.ui.comboBox.currentText()
+
+        machine = HashMachine()
+        encrypted_text = machine.encrypt(sha_func.lower(), text)
+
+        pyperclip.copy(encrypted_text)
+        self.ui.lineEdit.setText("")
+        QMessageBox.about(
+            self, "Encrypted", f"The encrypted password is: {encrypted_text}\nCopied to clipboard."
+        )
